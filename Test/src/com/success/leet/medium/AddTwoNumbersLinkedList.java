6e4f74c0194724359759bd03f8ec495ddc165c31
Tamil <tamil@tamils.rocks>
2021-07-26 19:35:06 -0400

    more leetcode programs

--- Test/src/com/success/leet/medium/AddTwoNumbersLinkedList.java
+++ Test/src/com/success/leet/medium/AddTwoNumbersLinkedList.java
@@ -11,75 +11,60 @@ package com.success.leet.medium;
 public class AddTwoNumbersLinkedList {
   public static void main(String[] args) {
 
-    ListNode1 l1 = new ListNode1(9);
-    ListNode1 l11 = new ListNode1(9);
+    ListNode l1 = new ListNode(9);
+    ListNode l11 = new ListNode(9);
     l1.next = l11;
 
-    l11.next = new ListNode1(2);
+    l11.next = new ListNode(2);
     l11 = l11.next;
 
-    l11.next = new ListNode1(1);
+    l11.next = new ListNode(1);
 
-    ListNode1 l2 = new ListNode1(9);
-    ListNode1 l22 = new ListNode1(9);
+    ListNode l2 = new ListNode(9);
+    ListNode l22 = new ListNode(9);
     l2.next = l22;
 
-    //    l22.next = new ListNode1(2);
+    //    l22.next = new ListNode(2);
     //    l22 = l22.next;
 
-    //    l22.next = new ListNode1(1);
+    //    l22.next = new ListNode(1);
 
     new Solution().addTwoNumbers(l1, l2);
   }
-}
 
-class ListNode1 {
-  int val;
-  ListNode1 next;
+  public static class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-  ListNode1() {}
+      ListNode rn = new ListNode();
+      ListNode rn1 = rn;
 
-  ListNode1(int val) {
-    this.val = val;
-  }
+      int sum = l1.val + l2.val;
+      int reminder = sum / 10;
+      rn.val = sum % 10;
 
-  ListNode1(int val, ListNode1 next) {
-    this.val = val;
-    this.next = next;
-  }
-}
+      l1 = l1.next;
+      l2 = l2.next;
 
-class Solution {
-  public ListNode1 addTwoNumbers(ListNode1 l1, ListNode1 l2) {
+      while (l1 != null || l2 != null) {
 
-    ListNode1 rn = new ListNode1();
-    ListNode1 rn1 = rn;
+        int n1 = l1 != null ? l1.val : 0;
+        int n2 = l2 != null ? l2.val : 0;
+        l1 = l1 != null ? l1.next : null;
+        l2 = l2 != null ? l2.next : null;
 
-    int sum = l1.val + l2.val;
-    int reminder = sum / 10;
-    rn.val = sum % 10;
+        sum = n1 + n2 + (reminder > 0 ? reminder : 0);
+        reminder = sum / 10;
 
-    l1 = l1.next;
-    l2 = l2.next;
+        rn1.next = new ListNode(sum % 10);
+        rn1 = rn1.next;
+      }
 
-    while (l1 != null || l2 != null) {
+      if (reminder > 0) {
+        rn1.next = new ListNode(1);
+      }
 
-      int n1 = l1 != null ? l1.val : 0;
-      int n2 = l2 != null ? l2.val : 0;
-      l1 = l1 != null ? l1.next : null;
-      l2 = l2 != null ? l2.next : null;
-
-      sum = n1 + n2 + (reminder > 0 ? reminder : 0);
-      reminder = sum / 10;
-
-      rn1.next = new ListNode1(sum % 10);
-      rn1 = rn1.next;
+      return rn;
     }
 
-    if (reminder > 0) {
-      rn1.next = new ListNode1(1);
-    }
-
-    return rn;
   }
 }
