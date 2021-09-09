211328f9697aaec5029f88d1f9c9b96a4716723d
Tamil <tamil@tamils.rocks>
2021-09-08 22:07:04 -0400

    socket server to hanlde multiple clients - test

--- Test/src/com/success/socket/Server.java
+++ Test/src/com/success/socket/Server.java
@@ -1,54 +1,126 @@
 package com.success.socket;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server{
+public class Server {
 
   private ServerSocket serverSocket;
-  
-  public Server() throws IOException{
+
+  public Server() throws IOException {
     serverSocket = new ServerSocket(8888);
     System.out.println("Server - Server socket object created");
   }
-  
-  public void run(){
+
+  public void run() {
     int counter = 0;
     try {
-      while(counter < 5){
+      while (counter < 5) {
         System.out.println("Server - before accepting connections");
         Socket server = serverSocket.accept();
-        
+
         System.out.println("Server - after accepting connections");
         DataInputStream in = new DataInputStream(server.getInputStream());
-        System.out.println("Server - messsage received from client-> "+in.readUTF());
-        
+        System.out.println("Server - messsage received from client-> " + in.readUTF());
+
         DataOutputStream dataOut = new DataOutputStream(server.getOutputStream());
-        System.out.println("Server - before sending message to client -> "+counter);
-        dataOut.writeUTF("Hello from server.. counter "+counter);
-        System.out.println("Server - after sending message to client -> "+counter);
+        System.out.println("Server - before sending message to client -> " + counter);
+        dataOut.writeUTF("Hello from server.. counter " + counter);
+        System.out.println("Server - after sending message to client -> " + counter);
         server.close();
         System.out.println("Server - server closed");
-        counter ++;
+        counter++;
       }
-    }
-    catch (Exception e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
-  
-  public static void main(String[] args){
-    new Thread(()->{
-      try {
-        new Server().run();
+
+  public void handleMultiClients() {
+    try {
+      while (true) {
+        // for each client, a new socket is opened. the above run method can handle only one client
+        // at a time
+        new ClientHandler(serverSocket.accept()).start();
       }
-      catch (IOException e) {
-        e.printStackTrace();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    int counter = 0;
+    try {
+      while (counter < 5) {
+        System.out.println("Server - before accepting connections");
+        Socket server = serverSocket.accept();
+
+        System.out.println("Server - after accepting connections");
+        DataInputStream in = new DataInputStream(server.getInputStream());
+        System.out.println("Server - messsage received from client-> " + in.readUTF());
+
+        DataOutputStream dataOut = new DataOutputStream(server.getOutputStream());
+        System.out.println("Server - before sending message to client -> " + counter);
+        dataOut.writeUTF("Hello from server.. counter " + counter);
+        System.out.println("Server - after sending message to client -> " + counter);
+        server.close();
+        System.out.println("Server - server closed");
+        counter++;
       }
-    }).start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
+  public static void main(String[] args) {
+    new Thread(
+            () -> {
+              try {
+                //                new Server().run();
+                new Server().handleMultiClients();
+              } catch (IOException e) {
+                e.printStackTrace();
+              }
+            })
+        .start();
+  }
+
+  private static class ClientHandler extends Thread {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public ClientHandler(Socket socket) {
+      this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+      try {
+        // write whatever logic here to receive/send data from clients
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+          if (".".equals(inputLine)) {
+            out.println("bye");
+            break;
+          }
+          out.println(inputLine);
+        }
+
+        in.close();
+        out.close();
+        clientSocket.close();
+
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
 }
